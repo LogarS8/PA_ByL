a6e54dd1a4f34af15228e10850560408ff7f571c
vDemian0w00 <cfajardoa2000@alumno.ipn.mx>
2022-11-08 17:31:48 -0600

    cambio docente 2

--- src/DB/DB.js
+++ src/DB/DB.js
@@ -10,17 +10,17 @@ import {
 
 
 export const pool = createPool({
-  // host: DB_HOST,
-  // user: DB_USER,
-  // // port: DB_PORT,
-  // password: DB_PASSWORD,
-  // database: DB_NAME,
-  // ssl:{
-  //   rejectUnauthorized: false
-  // },
-  host: "localhost",
-  user: "root",
-  port: 3306,
-  password: "n0m3l0",
-  database: "parodri",
+  host: DB_HOST,
+  user: DB_USER,
+  // port: DB_PORT,
+  password: DB_PASSWORD,
+  database: DB_NAME,
+  ssl:{
+    rejectUnauthorized: false
+  },
+  // host: "localhost",
+  // user: "root",
+  // port: 3306,
+  // password: "n0m3l0",
+  // database: "parodri",
 });
